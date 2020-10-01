a08f20e3e758555d4a13c9d4f8f39f77ff897362
mainprograming <63953689+mainprograming@users.noreply.github.com>
2020-10-01 16:33:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+
+ $('.header-slick').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  speed: 2000,
+  autoplaySpeed: 3000,
+  autoplay: true,
+ });
+
+});
